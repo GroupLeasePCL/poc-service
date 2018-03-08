dec389ddb9c8f6907090b3ad9620e7a092c5374a
Peeranut <peeranut.ng@grouplease.co.th>
2018-03-08 09:46:05 +0700

    [Project] Init spring boot service project

---
+++ src/main/java/th/co/grouplease/pocservice/PocServiceApplication.java
@@ -0,0 +1,12 @@
+package th.co.grouplease.pocservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/th/co/grouplease/pocservice/PocServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package th.co.grouplease.pocservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
