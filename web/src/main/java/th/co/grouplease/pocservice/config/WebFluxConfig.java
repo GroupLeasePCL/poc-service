916f1960caf2ef045c13c317e91b060ffaede8a8
Peeranut <peeranut.ng@grouplease.co.th>
2018-03-11 18:53:32 +0700

    [Config] Enable cross-origin request handling for all pattern (Note that this shall not be a production configuration)

---
+++ web/src/main/java/th/co/grouplease/pocservice/config/WebFluxConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Group Lease Public Company Limited. All rights reserved. (http://www.grouplease.co.th/)
+ * Author: Peeranut Ngaorungsri (peeranut.ng@grouplease.co.th) 3/11/18 6:44 PM
+ */
+
+package th.co.grouplease.pocservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*");
+  }
+}
